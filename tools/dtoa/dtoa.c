1d4e7bf5c5b6540af29b78b830024b495f6a7e73
Paul Koning <ni1d@arrl.net>
2004-01-06 21:25:04 +0000

    make i64/u64 declaration work for gcc
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@93 d8f3d347-7192-4e35-a67b-6d701703d080

--- tools/dtoa/dtoa.c
+++ tools/dtoa/dtoa.c
@@ -55,8 +55,13 @@ typedef signed long  i32;
 typedef unsigned char  u8;
 typedef unsigned short u16;
 typedef unsigned long  u32;
+#ifdef __GNUC__
+typedef long long i64;
+typedef unsigned long long u64;
+#else
 typedef __int64 i64;
 typedef unsigned __int64 u64;
+#endif
 #ifndef __cplusplus
 typedef int bool;
 #endif
