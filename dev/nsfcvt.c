d3cf07cc1d4f8edb2899498a3b1e94cacfa2508e
Paul Koning <ni1d@arrl.net>
2004-07-14 20:35:44 +0000

    new tool for converting Steve's NSF pack images to DtCyber format
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@199 d8f3d347-7192-4e35-a67b-6d701703d080

---
+++ dev/nsfcvt.c
@@ -0,0 +1,133 @@
+/*--------------------------------------------------------------------------
+**
+**  Copyright (c) 2003-2004, Tom Hunter, Gerard van der Grinten, Paul Koning
+**  (see license.txt)
+**
+**  Name: dd8xxcvt.c
+**
+**  Description:
+**      Format conversion for dd8xx image files
+**
+**--------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+
+#define SectorSize              322
+#define SectorBytes             512
+#define Mask12  07777
+
+typedef unsigned char u8;
+typedef unsigned short PpWord;
+typedef unsigned long long CpWord;
+
+PpWord secbuf[SectorSize];
+u8 sec[SectorSize * 2];
+u8 wsec[SectorSize * 2];
+
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Read sector to a local buffer
+**
+**  Parameters:     Name        Description.
+**                  fcb         FILE struct for input disk image file
+**                  buf         buffer pointer (PpWord[322])
+**
+**  Returns:        Nothing.
+**
+**  This routine reads the sector set by the most recent seek.
+**  Note that it does NOT advance the current sector.
+**
+**------------------------------------------------------------------------*/
+static int dd8xxReadSector(FILE *fcb, void *buf)
+{
+    int i, j;
+    u8 *bp;
+    PpWord *pp;
+    int sectorSize;
+    CpWord t;
+    
+    sectorSize = SectorBytes;
+
+    if (fread(sec, 1, sectorSize, fcb) != sectorSize)
+        return 0;
+    
+    bp = sec;
+    pp = (PpWord *) buf;
+    *pp++ = 0;
+    *pp++ = 0;
+    
+    for (i = 0; i < 64 * 8; i += 8)
+    {
+        t = 0;
+        for (j = 0; j < 8; j++)
+            t = (t << 8) + *bp++;
+        for (j = 0; j < 5; j++)
+        {
+            *pp++ = (t >> 48) & 07777;
+            t <<= 12;
+        }
+    }
+    return 1;
+}
+
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Write sector from a local buffer
+**
+**  Parameters:     Name        Description.
+**                  fcb         FILE struct for input disk image file
+**                  buf         buffer pointer (PpWord[322])
+**
+**  Returns:        Nothing.
+**
+**  This routine writes the sector set by the most recent seek.
+**  Note that it does NOT advance the current sector.
+**
+**------------------------------------------------------------------------*/
+static void dd8xxWriteSector(FILE *fcb, const void *buf)
+{
+    int i;
+    u8 *bp;
+    const PpWord *pp;
+    int sectorSize;
+    
+    sectorSize = SectorBytes;
+    
+    bp = wsec;
+    pp = (const PpWord *) buf;
+    for (i = 0; i < SectorSize; i += 2)
+    {
+        *bp++ = *pp >> 4;
+        *bp = *pp++ << 4;
+        *bp++ |= *pp >> 8;
+        *bp++ = *pp++;
+    }
+    if (fwrite(wsec, 1, sectorSize, fcb) != sectorSize)
+    {
+        perror ("dd8xxWriteSector");
+        exit (1);
+    }
+}
+
+int main (int argc, char **argv)
+{
+    FILE *inf, *outf;
+    inf = fopen (argv[1], "rb");
+    if (inf == 0)
+    {
+        perror ("input file open");
+        exit (1);
+    }
+    outf = fopen (argv[2], "wb");
+    if (outf == 0)
+    {
+        perror ("output file open");
+        exit (1);
+    }
+    while (dd8xxReadSector (inf, secbuf))
+        dd8xxWriteSector (outf, secbuf);
+    fclose (inf);
+    fclose (outf);
+    return 0;
+}
