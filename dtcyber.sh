126b87c21b555f0d7fbe3bcdc8a8c1c6f6b5e9f0
Paul Koning <ni1d@arrl.net>
2004-11-23 22:46:15 +0000

    enable corefile
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@307 d8f3d347-7192-4e35-a67b-6d701703d080

--- dtcyber.sh
+++ dtcyber.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # first start dtcyber
+ulimit -c unlimited
 ./dtcyber cybis580auto &
 sleep 1
 
 # now start one each of the UIs
-./dd60 .1 &
+./dd60 .06 &
 ./dtoper &
 ./pterm localhost 5005 &
