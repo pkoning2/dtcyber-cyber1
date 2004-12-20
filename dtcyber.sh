df57da9bd70c7af9bc8d8248faf645616b98dfc7
Paul Koning <ni1d@arrl.net>
2004-12-20 15:56:35 +0000

    wait until niu initializaiton is done before starting the UI components
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@346 d8f3d347-7192-4e35-a67b-6d701703d080

--- dtcyber.sh
+++ dtcyber.sh
@@ -3,7 +3,13 @@
 # first start dtcyber
 ulimit -c unlimited
 ./dtcyber cybis580auto | tee cyberlog &
-sleep 1
+
+# wait for the NIU port to appear (that's the last one of the UI ports
+# to be initialized)
+until netstat -tna | fgrep -q 127.0.0.1:5005; do
+    sleep 1
+    echo -n "."
+done
 
 # now start one each of the UIs
 ./dd60 .06 &
