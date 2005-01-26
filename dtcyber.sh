5910210baf8af85cdfbd1d20551fa912ad535db9
Paul Koning <ni1d@arrl.net>
2005-01-26 20:45:25 +0000

    make it a bit nicer
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@378 d8f3d347-7192-4e35-a67b-6d701703d080

--- dtcyber.sh
+++ dtcyber.sh
@@ -2,16 +2,26 @@
 
 # first start dtcyber
 ulimit -c unlimited
-./dtcyber cybis580auto | tee cyberlog &
+if [ "$1" = "" ]; then
+    ./dtcyber cybis580auto | tee cyberlog &
+else
+    ./dtcyber $1 &
+fi
 
-# wait for the NIU port to appear (that's the last one of the UI ports
-# to be initialized)
-until netstat -tna | fgrep -q 127.0.0.1:5005; do
+echo dtcyber started
+
+# wait for the operator port to appear
+until netstat -tna | fgrep -q 127.0.0.1:5006; do
     sleep 1
     echo -n "."
 done
 
+echo done waiting
+
 # now start one each of the UIs
 ./dd60 .06 &
 ./dtoper &
-./pterm localhost 5005 &
+
+echo ui started
+
+# ./pterm localhost 5005 &
