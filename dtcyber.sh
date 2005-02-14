fee6707e2cb2b019ec280af63049bebcf5220f94
Paul Koning <ni1d@arrl.net>
2005-02-14 21:08:14 +0000

    make more tolerant of startup variations
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@391 d8f3d347-7192-4e35-a67b-6d701703d080

--- dtcyber.sh
+++ dtcyber.sh
@@ -3,25 +3,34 @@
 # first start dtcyber
 ulimit -c unlimited
 if [ "$1" = "" ]; then
-    ./dtcyber cybis580auto | tee cyberlog &
+    ./dtcyber cybis871auto | tee cyberlog &
 else
     ./dtcyber $1 &
 fi
 
 echo dtcyber started
 
-# wait for the operator port to appear
+# wait for each of the ports to appear
+until netstat -tna | fgrep -q 127.0.0.1:5005; do
+    sleep 1
+    echo -n "."
+done
 until netstat -tna | fgrep -q 127.0.0.1:5006; do
     sleep 1
     echo -n "."
 done
+until netstat -tna | fgrep -q 127.0.0.1:5007; do
+    sleep 1
+    echo -n "."
+done
 
-echo done waiting
+sleep 1
+
+echo ""
 
 # now start one each of the UIs
 ./dd60 .06 &
 ./dtoper &
+# ./pterm localhost 5005 &
 
 echo ui started
-
-# ./pterm localhost 5005 &
