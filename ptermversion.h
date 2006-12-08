8e8e8cde622f574ea8cb4c71910129bc09d85d46
Joe Stanton <joe@cyber1.org>
2006-12-08 22:08:24 +0000

    Prefs and Connection dialog UI element changes for sizing for Linux/Mac.
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@725 d8f3d347-7192-4e35-a67b-6d701703d080

--- ptermversion.h
+++ ptermversion.h
@@ -4,4 +4,4 @@
 #define STRPRODUCTVER       "4, 0, 8, 4"
 #define STRFILEDESC         "PLATO Terminal Emulator"
 #define STRPRODUCTNAME      "PLATO Terminal Emulator"
-#define STRLEGALCOPYRIGHT   "Copyright © 2004-2006, Paul Koning"
+#define STRLEGALCOPYRIGHT   "Copyright \xA9 2004-2006, Paul Koning"
