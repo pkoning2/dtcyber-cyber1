06fe6699df0888682115a7a85d89f83f12ec9515
Paul Koning <ni1d@arrl.net>
2007-02-09 20:34:05 +0000

    Add ASCII support; allow changing the message on the fly
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@749 d8f3d347-7192-4e35-a67b-6d701703d080

--- blackbox.c
+++ blackbox.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------------
 **
-**  Copyright (c) 2005, Paul Koning, Tom Hunter
+**  Copyright (c) 2005-2007, Paul Koning, Tom Hunter
 **
 **  Name: blackbox.c
 **
@@ -29,99 +29,345 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
 #include "const.h"
 #include "types.h"
 #include "proto.h"
 
-ChSlot ic, oc;
-DevSlot id, od;
-ChSlot *activeChannel;
-DevSlot *activeDevice;
-u32 rtcClock = 0;
+long platoConns = 128;
+long platoPort = 5004;
+long asciiConns = 64;
+long asciiPort = 8005;
 bool emulationActive = TRUE;
 
-long platoLocalConns = 31;
-long platoLocalPort = 5005;
-long platoConns = 96;
-long platoPort = 5004;
+int currentY = 128;
+char *msg;
 
-extern void niuSendstr(int stat, const char *p);
-extern void niuSendWord(int stat, int word);
+static NetPortSet niuPorts;
+static NetPortSet niuAsciiPorts;
 
-DevSlot *channelAttach (u8 channelNo, u8 eqNo, u8 devType)
-    {
-    if (channelNo == 1)
-        {
-        activeDevice = &id;
-        }
-    else
-        {
-        activeDevice = &od;
-        }
-    activeDevice->devType = devType;
-    activeDevice->eqNo = eqNo;
-    return activeDevice;
-    }
-
-
-void * opInitStatus (const char *type, int ch, int un)
-    {
-    return NULL;
-    }
-
-void opSetStatus (void *buf, const char *msg)
-    {
-    }
+static void niuSendstr(int stat, const char *p);
+static void niuSendWord(int stat, int word);
+static void niuAsciiWelcome(NetPort *np, int stat);
+static void niuRemoteWelcome(NetPort *np, int stat);
+static void bbSendNiu(int stat);
+static void bbSendAscii(int stat);
 
 int main (int argc, char **argv)
     {
-    int stat;
-    char *msg;
+    int stat, i;
+    NetPort *np;
+    char msg2[200];
     
     if (argc < 2)
         {
-        msg = "PLATO is down for the moment";
+        msg = strdup ("PLATO is down for the moment");
         }
     else
         {
-        msg = argv[1];
+        msg = strdup (argv[1]);
         }
     
-    ic.id = 1;
-    oc.id = 2;
-    id.channel = &ic;
-    od.channel = &oc;
+    /*
+    **  Create the threads which will deal with TCP connections.
+    */
+    niuPorts.portNum = platoPort;
+    niuPorts.maxPorts = platoConns;
+    niuPorts.localOnly = FALSE;
+    niuPorts.callBack = niuRemoteWelcome;
+    niuAsciiPorts.portNum = asciiPort;
+    niuAsciiPorts.maxPorts = asciiConns;
+    niuAsciiPorts.localOnly = FALSE;
+    niuAsciiPorts.callBack = niuAsciiWelcome;
     
-    niuInit (0, 1, 2, NULL);
-    activeDevice = &id;
-    (*id.func) (041);
-    activeDevice = &od;
-    (*od.func) (0);
+    dtCreateListener (&niuPorts, 1024);
+    dtCreateListener (&niuAsciiPorts, 1024);
+
+    printf ("Current message is: '%s'\n", msg);
+    printf ("Enter a new message at any time, or Ctrl-D to stop blackbox.\n");
+    fcntl (fileno (stdin), F_SETFL, O_NONBLOCK);
     
     for (;;)
         {
         /*
-        **  Read and discard any input; this also causes niu.c to
-        **  handle disconnects.
+        **  Read and discard any input; also handle disconnects.
         */
         for (;;)
             {
-            ic.full = FALSE;
-            activeDevice = &id;
-            activeChannel = &ic;
-            (*id.io) ();
-            if (!ic.full)
+            np = dtFindInput (&niuPorts, 0);
+            if (np == NULL)
                 {
                 break;
                 }
+            i = dtRead  (&np->fet, -1);
+            if (i < 0)
+                {
+                dtClose (np, &niuPorts);
+                }
             }
-        for (stat = 1; stat < platoLocalConns + platoConns + 32; stat++)
+        for (;;)
             {
-            niuSendWord (stat, 0100033);        /* mode 3, mode rewrite, screen */
-            niuSendWord (stat, 0201400);        /* load Y = 256 */
-            niuSendWord (stat, 0200060);        /* load X = 48 */
-            niuSendstr (stat, msg);
+            np = dtFindInput (&niuAsciiPorts, 0);
+            if (np == NULL)
+                {
+                break;
+                }
+            i = dtRead  (&np->fet, -1);
+            if (i < 0)
+                {
+                dtClose (np, &niuAsciiPorts);
+                }
             }
+        for (stat = 0; stat < platoConns; stat++)
+            {
+            bbSendNiu (stat);
+            }
+        for (stat = 0; stat < asciiConns; stat++)
+            {
+            bbSendAscii (stat);
+            }
+        currentY -= 32;
+        if (currentY < 64)
+            {
+            currentY = 512 - 64;
+            }
+
+        /*
+        **  Check for a new message, or exit.
+        */
         sleep (5);
+        i = read (fileno (stdin), msg2, sizeof (msg2));
+        if (i == 0)
+            {
+            return 0;
+            }
+        else if (i > 0)
+            {
+            free (msg);
+            msg2[i - 1] ='\0';
+            msg = strdup (msg2);
+            }
         }
+    }
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Send a string to a station
+**
+**  Parameters:     Name        Description.
+**                  stat        Station number
+**                  p           pointer to ASCII string
+**
+**  Returns:        nothing.
+**
+**------------------------------------------------------------------------*/
+static void niuSendstr(int stat, const char *p)
+    {
+    int cc = 2;
+    int w = 017720;
+    bool shift = FALSE;
+    int c;
+    
+    if (&(niuPorts.portVec + stat)->fet.connFd == 0)
+        {
+        return;
+        }
+    
+    while ((c = *p++) != 0)
+        {
+        if (isupper (c))
+            {
+            c = tolower (c);
+            if (!shift)
+                {
+                w = (w << 6 | 077);
+                if (++cc == 3)
+                    {
+                    cc = 0;
+                    niuSendWord (stat, w);
+                    niuSendWord (stat, 1);                    
+                    w = 1;
+                    }
+                w = (w << 6 | 021);
+                if (++cc == 3)
+                    {
+                    cc = 0;
+                    niuSendWord (stat, w);
+                    niuSendWord (stat, 1);                    
+                    w = 1;
+                    }
+                shift = TRUE;
+                }
+            }
+        else if (shift)
+            {
+            w = (w << 6 | 077);
+            if (++cc == 3)
+                {
+                cc = 0;
+                niuSendWord (stat, w);
+                niuSendWord (stat, 1);                    
+                w = 1;
+                }
+            w = (w << 6 | 020);
+            if (++cc == 3)
+                {
+                cc = 0;
+                niuSendWord (stat, w);
+                niuSendWord (stat, 1);                    
+                w = 1;
+                }
+            shift = FALSE;
+            }
+        w = (w << 6 | asciiToCdc[c]);
+        if (++cc == 3)
+            {
+            cc = 0;
+            niuSendWord (stat, w);
+            niuSendWord (stat, 1);                    
+            w = 1;
+            }
+        }
+    if (cc > 0)
+        {
+        while (cc < 3)
+            {
+            w = (w << 6 | 077);
+            cc++;
+            }
+        niuSendWord (stat, w);
+        niuSendWord (stat, 1);                    
+        }
+    }
+
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Send an output word to a station
+**
+**  Parameters:     Name        Description.
+**                  stat        Station number
+**                  word        NIU data word
+**
+**  Returns:        nothing.
+**
+**------------------------------------------------------------------------*/
+static void niuSendWord(int stat, int word)
+    {
+    NetFet *fet;
+    u8 data[3];
+
+    fet = &(niuPorts.portVec + stat)->fet;
+    if (fet->connFd == 0)
+        {
+        return;
+        }
+    
+    data[0] = word >> 12;
+    data[1] = ((word >> 6) & 077) | 0200;
+    data[2] = (word & 077) | 0300;
+
+    dtSend(fet, data, 3);
+    }
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Handle connect/disconnect
+**
+**  Parameters:     Name        Description.
+**                  np          NetPort pointer
+**                  stat        station number
+**
+**  Returns:        nothing.
+**
+**------------------------------------------------------------------------*/
+static void niuRemoteWelcome(NetPort *np, int stat)
+    {
+    if (np->fet.connFd == 0)
+        {
+        /*
+        **  Connection was dropped.
+        */
+        return;
+        }
+
+    /*
+    **  New connection for this port.  Send a message right now
+    */
+    bbSendNiu (stat);
+    }
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Handle connect/disconnect
+**
+**  Parameters:     Name        Description.
+**                  np          NetPort pointer
+**                  stat        station number
+**
+**  Returns:        nothing.
+**
+**------------------------------------------------------------------------*/
+static void niuAsciiWelcome(NetPort *np, int stat)
+    {
+    if (np->fet.connFd == 0)
+        {
+        /*
+        **  Connection was dropped.
+        */
+        return;
+        }
+
+    /*
+    **  New connection for this port.  Send a message right now
+    */
+    bbSendAscii (stat);
+    }
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Send the message to a station, NIU format
+**
+**  Parameters:     Name        Description.
+**                  stat        station number
+**
+**  Returns:        nothing.
+**
+**------------------------------------------------------------------------*/
+static void bbSendNiu (int stat)
+    {
+    niuSendWord (stat, 0100033);            /* mode 3, mode rewrite, screen */
+    niuSendWord (stat, 0201000 + currentY); /* load Y = currentY */
+    niuSendWord (stat, 0200060);            /* load X = 48 */
+    niuSendstr (stat, msg);
+    }
+
+/*--------------------------------------------------------------------------
+**  Purpose:        Send the message to a station, ASCII format
+**
+**  Parameters:     Name        Description.
+**                  stat        station number
+**
+**  Returns:        nothing.
+**
+**------------------------------------------------------------------------*/
+static void bbSendAscii (int stat)
+    {
+    NetFet *fet;
+    const char *modeStr = "\033\002"    // Enter PLATO mode
+                          "\033\014"    // Full erase
+                          "\033\024"    // Mode rewrite
+                          "\0332";      // Load coordinate
+    char setY[2];
+    const char *setX = "\041\120";      // X = 48
+    
+    fet = &(niuAsciiPorts.portVec + stat)->fet;
+    if (fet->connFd == 0)
+        {
+        return;
+        }
+
+    dtSend(fet, modeStr, strlen (modeStr));
+    setY[0] = (currentY >> 5) | 040;
+    setY[1] = (currentY & 037) | 0140;
+    dtSend (fet, setY, 2);
+    dtSend (fet, setX, 2);
+    
+    dtSend (fet, msg, strlen (msg));
     }
