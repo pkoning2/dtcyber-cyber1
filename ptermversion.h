22f9a0eed60602e31f7dca339d6581976b77c1e5
Joe Stanton <joe@cyber1.org>
2007-02-21 23:34:11 +0000

    Changed version to 4.1.0 for release.  Changed copyright to include 2007.
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@759 d8f3d347-7192-4e35-a67b-6d701703d080

--- ptermversion.h
+++ ptermversion.h
@@ -1,7 +1,7 @@
-#define FILEVER             4,0,9
-#define PRODUCTVER          4,0,9
-#define STRFILEVER          "4.0.9"
-#define STRPRODUCTVER       "4.0.9"
+#define FILEVER             4,1,0
+#define PRODUCTVER          4,1,0
+#define STRFILEVER          "4.1.0"
+#define STRPRODUCTVER       "4.1.0"
 #define STRFILEDESC         "PLATO Terminal Emulator"
 #define STRPRODUCTNAME      "PLATO Terminal Emulator"
-#define STRLEGALCOPYRIGHT   "Copyright \xA9 2004-2006, Paul Koning"
+#define STRLEGALCOPYRIGHT   "Copyright \xA9 2004-2007, Paul Koning"
