72ea02926587614cbf10f042db73c2965df6e2b0
Paul Koning <ni1d@arrl.net>
2007-06-26 15:57:38 +0000

    Add dd60 support, plus some bugfixes
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@809 d8f3d347-7192-4e35-a67b-6d701703d080

--- dtcontrol.py
+++ dtcontrol.py
@@ -95,9 +95,9 @@ class Oper (Connection, threading.Thread):
     a thread that collects data from DtCyber and updates local
     state to reflect what it hears.
     """
-    def __init__ (self, host = "localhost", port = 5006):
+    def __init__ (self, port = 5006):
         threading.Thread.__init__ (self)
-        Connection.__init__ (self, host, port)
+        Connection.__init__ (self, "localhost", port)
         self.settimeout (5)
         self.stopnow = False
         self.fixedtext = elist ()
@@ -149,13 +149,6 @@ class Oper (Connection, threading.Thread):
                     i = ord (v[0])
                     v = v[1:]
                     self.status[i] = v
-                    fields = v.split (None, 3)
-                    if len (fields) >= 3:
-                        skey = (int (fields[1], 8), int (fields[2], 8))
-                        try:
-                            self.statusdict[skey] = fields[3]
-                        except IndexError:
-                            self.statusdict[skey] = ""
                     if i == 0:
                         # System status line, update locked and debug status
                         self.locked = not v.startswith ("UNLOCKED")
@@ -164,6 +157,14 @@ class Oper (Connection, threading.Thread):
                             self.operators = int (v[15:].split ()[0])
                         else:
                             self.operators = 1
+                    else:
+                        fields = v.split (None, 3)
+                        if len (fields) >= 3:
+                            skey = (int (fields[1], 8), int (fields[2], 8))
+                            try:
+                                self.statusdict[skey] = fields[3]
+                            except IndexError:
+                                self.statusdict[skey] = ""
             except socket.timeout:
                 pass
             except EOFError:
@@ -310,6 +311,20 @@ class Pterm (Connection, threading.Thread):
         self.sendall (data)
         self.arrow = False
 
+    def waitarrow (self, wait):
+        """Wait the specified number of seconds for an arrow to appear.
+        Returns True if there was an arrow, False if not.
+        """
+        delay = wait / 10.0
+        if delay > 1:
+            delay = 1
+        while wait > 0:
+            if self.arrow:
+                return True
+            time.sleep (delay)
+            wait -= delay
+        return False
+
     # This table is nearly the same as the one in charset.c, except
     # for the entries for ctrl-i, ctrl-j, and rubout, which are
     # TAB, NEXT, and ERASE respectively.
@@ -347,20 +362,6 @@ class Pterm (Connection, threading.Thread):
           -1,    0116116,0117121,0117105,0117130, -1, 0117125,  0013,
           -1,    0125121,0125105,0125130,0125125,0131105, -1,   0131125
        )
-
-    def waitarrow (self, wait):
-        """Wait the specified number of seconds for an arrow to appear.
-        Returns True if there was an arrow, False if not.
-        """
-        delay = wait / 10.0
-        if delay > 1:
-            delay = 1
-        while wait > 0:
-            if self.arrow:
-                return True
-            time.sleep (delay)
-            wait -= delay
-        return False
 
     def sendstr (self, s, term = None, wait = None):
         """Send a text string, converting from ASCII as we go.
@@ -472,3 +473,183 @@ class Pterm (Connection, threading.Thread):
             self.sendkey (self.SHIFT + self.STOP)
             self.waitarrow (4)
             
+
+class Dd60 (Connection, threading.Thread):
+    """A connection to the DtCyber console (green tubes).  It includes
+    a thread that collects data from DtCyber and updates local
+    state to reflect what it hears.
+    """
+    # A blank line
+    line64 = 64 * ' '
+
+    # Mode codes
+    LEFT = 0
+    RIGHT = 4
+    SMALL = 0
+    MEDIUM = 1
+    LARGE = 2
+    DOT = 3
+
+    # Command codes (output stream non-data codes).  Unlisted codes
+    # are ignored.
+    SETX = 0200
+    SETY = 0210
+    SETMODE = 0240
+    ENDBLOCK = 0250
+    
+    def __init__ (self, port = 5007, interval = 3):
+        threading.Thread.__init__ (self)
+        Connection.__init__ (self, "localhost", port)
+        self.settimeout (5)
+        self.erase ()
+        self.stopnow = False
+        self.interval = -1
+        self.setinterval (interval)
+        self.sendkey (070)                 # XON to start output flow
+        self.mode = self.DOT + self.LEFT   # no display until mode set
+        self.x = self.y = 0
+        self.endblock = True
+        self.start ()
+
+    def erase (self):
+        self.left = [ ]
+        self.right = [ ]
+        for i in xrange (32):
+            self.left.append (self.line64)
+            self.right.append (self.line64)
+
+    def next (self):
+        """Return next byte of Cyber output, as an integer.
+        """
+        return ord (self.read (1))
+    
+    def run (self):
+        """Collect data from DtCyber, and store it away into the
+        left or right screen arrays.
+        """
+        while True:
+            try:
+                if self.stopnow:
+                    self.close ()
+                    break
+                ch = self.next ()
+                self.process_output (ch)
+            except socket.timeout:
+                pass
+            except EOFError:
+                break
+        self.stopnow = True
+
+    def stop (self):
+        if not self.stopnow:
+            self.stopnow = True
+            self.join ()
+
+    def sendkey (self, key):
+        self.sendall (chr (key))
+        
+    def setinterval (self, interval):
+        if self.interval != interval:
+            self.interval = interval
+            if interval > 63:
+                interval = 63
+            if interval < 1:
+                self.sendkey (0200 + int (interval * 50))
+            else:
+                self.sendkey (0300 + int (interval))
+
+    # This table is nearly the same as the one in charset.c, except
+    # that the entry for rubout is ERASE.
+    asciiToConsole = \
+    (   0,      0,      0,      0,      0,      0,      0,      0,
+        061,    0,      060,    0,      0,      060,    0,      0,
+        0,      0,      0,      0,      0,      0,      0,      0,
+        0,      0,      0,      0,      0,      0,      0,      0,
+        062,    0,      0,      0,      0,      0,      0,      0,
+        051,    052,    047,    045,    056,    046,    057,    050,
+        033,    034,    035,    036,    037,    040,    041,    042,
+        043,    044,    0,      0,      0,      054,    0,      0,
+        0,      01,     02,     03,     04,     05,     06,     07,
+        010,    011,    012,    013,    014,    015,    016,    017,
+        020,    021,    022,    023,    024,    025,    026,    027,
+        030,    031,    032,    053,    0,      055,    0,      0,
+        0,      01,     02,     03,     04,     05,     06,     07,
+        010,    011,    012,    013,    014,    015,    016,    017,
+        020,    021,    022,    023,    024,    025,    026,    027,
+        030,    031,    032,    0,      0,      0,      0,      061 )
+    
+    def sendstr (self, text):
+        """Send a text string, converting from ASCII as we go.
+        There is no delay in here, so don't send too much.
+        """
+        for c in text:
+            key = self.asciiToConsole[ord (c)]
+            if key == 0:
+                continue
+            self.sendkey (key)
+
+    def __repr__ (self):
+        return self.screen (1)
+
+    def screen (self, n):
+        if n:
+            return '\n'.join (self.right)
+        else:
+            return '\n'.join (self.left)
+
+    # This table comes from charset.c, replacing 0 (unused) entries
+    # by blank
+    consoleToAscii = \
+    ( ' ',    'A',    'B',    'C',    'D',    'E',    'F',    'G',
+      'H',    'I',    'J',    'K',    'L',    'M',    'N',    'O',
+      'P',    'Q',    'R',    'S',    'T',    'U',    'V',    'W',
+      'X',    'Y',    'Z',    '0',    '1',    '2',    '3',    '4',
+      '5',    '6',    '7',    '8',    '9',    '+',    '-',    '*',
+      '/',    '(',    ')',    ' ',    '=',    ' ',    ',',    '.',
+      ' ',    ' ',    ' ',    ' ',    ' ',    ' ',    ' ',    ' ',
+      ' ',    ' ',    ' ',    ' ',    ' ',    ' ',    ' ',    ' ' )
+
+    def process_char (self, ch):
+        """Process a data byte.  If not in dot mode, store it in the
+        correct position of the current line.  Medium and large chars
+        are handled by storing the char at the current X/Y and skipping
+        one (medium) or three (large) character positions.
+        """
+        if self.mode & self.RIGHT:
+            screen = self.left
+        else:
+            screen = self.right
+        mode = self.mode & 3
+        if mode != self.DOT:
+            line = screen[self.y]
+            
+            screen[self.y] = line[:self.x] + self.consoleToAscii[ch] + line[self.x + 1:]
+            x = self.x + 1
+            if mode == self.MEDIUM:
+                x += 1
+            elif mode == self.LARGE:
+                x += 3
+            self.x = x & 077
+
+    def process_output (self, ch):
+        """Process Cyber output data.  Sort control codes (upper bit set)
+        from data codes (upper bit clear).  Whenever we receive an output
+        byte after the end of block code, do a full erase to prepare the
+        screen image for another block of data.
+        """
+        if self.endblock:
+            self.erase ()
+            self.endblock = False
+        if ch & 0200:
+            # Control code
+            action = ch & 0270
+            if action == self.SETX:
+                self.x = (((ch << 8) + self.next ()) & 0770) / 8
+            elif action == self.SETY:
+                self.y = (((ch << 8) + self.next ()) & 0760) / 16
+            elif action == self.SETMODE:
+                self.mode = ch & 7
+            elif action == self.ENDBLOCK:
+                self.endblock = True
+        else:
+            self.process_char (ch)
