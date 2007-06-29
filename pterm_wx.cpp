ccfc8f364fa080bf810bffde64fbb2c3dfc36d4f
Joe Stanton <joe@cyber1.org>
2007-06-29 15:45:15 +0000

    Fix for -font default- handling.  Resolves issue shown in =edit= after pressing TERM/BACK/replot.  Error was that I had coded "default" as meaning 0, but Plato sends 1 as default.
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@814 d8f3d347-7192-4e35-a67b-6d701703d080

--- pterm_wx.cpp
+++ pterm_wx.cpp
@@ -5380,7 +5380,7 @@ void PtermFrame::SetFontFlags (int n)
 **------------------------------------------------------------------------*/
 void PtermFrame::SetFontActive ()
 {
-	m_usefont = m_fontface.Cmp(wxT("default")) != 0;
+	m_usefont = m_fontface.Cmp(wxT("default")) > 1;
 	if (m_usefont)
 	{
 		m_font = new wxFont;
