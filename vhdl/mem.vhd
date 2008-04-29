fa42bbf8517514471ed85422ed5bc3bfacc1a4e2
Paul Koning <ni1d@arrl.net>
2008-04-29 16:34:49 +0000

    correct connector order
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@893 d8f3d347-7192-4e35-a67b-6d701703d080

--- vhdl/mem.vhd
+++ vhdl/mem.vhd
@@ -55,60 +55,60 @@ use work.sigs.all;
 entity mem is
   
   port (
-    p122 : in std_logic;                -- addr(0)
-    p120 : in std_logic;                -- addr(1)
-    p108 : in std_logic;                -- addr(2)
-    p110 : in std_logic;                -- addr(3)
-    p7   : in std_logic;                -- addr(4)
-    p9   : in std_logic;                -- addr(5)
-    p13  : in std_logic;                -- addr(6)
-    p11  : in std_logic;                -- addr(7)
-    p112 : in std_logic;                -- addr(8)
-    p114 : in std_logic;                -- addr(9)
-    p118 : in std_logic;                -- addr(10)
-    p116 : in std_logic;                -- addr(11)
-    p29  : in std_logic;                -- wdata(0)
-    p5   : in std_logic;                -- wdata(1)
-    p106 : in std_logic;                -- wdata(2)
-    p126 : in std_logic;                -- wdata(3)
-    p27  : in std_logic;                -- wdata(4)
-    p1   : in std_logic;                -- wdata(5)
-    p128 : in std_logic;                -- wdata(6)
-    p104 : in std_logic;                -- wdata(7)
-    p3   : in std_logic;                -- wdata(8)
-    p25  : in std_logic;                -- wdata(9)
-    p130 : in std_logic;                -- wdata(10)
-    p102 : in std_logic;                -- wdata(11)
-    p101 : out std_logic;               -- rdata(0)
-    p103 : out std_logic;               -- rdatab(0)
-    p127 : out std_logic;               -- rdata(1)
-    p129 : out std_logic;               -- rdatab(1)
-    p28  : out std_logic;               -- rdata(2)
-    p30  : out std_logic;               -- rdatab(2)
-    p2   : out std_logic;               -- rdata(3)
-    p4   : out std_logic;               -- rdatab(3)
-    p105 : out std_logic;               -- rdata(4)
-    p107 : out std_logic;               -- rdatab(4)
-    p123 : out std_logic;               -- rdata(5)
-    p125 : out std_logic;               -- rdatab(5)
-    p6   : out std_logic;               -- rdata(6)
-    p8   : out std_logic;               -- rdatab(6)
-    p24  : out std_logic;               -- rdata(7)
-    p26  : out std_logic;               -- rdatab(7)
-    p119 : out std_logic;               -- rdata(8)
-    p121 : out std_logic;               -- rdatab(8)
-    p109 : out std_logic;               -- rdata(9)
-    p111 : out std_logic;               -- rdatab(9)
-    p10  : out std_logic;               -- rdata(10)
-    p12  : out std_logic;               -- rdatab(10)
-    p20  : out std_logic;               -- rdata(11)
-    p22  : out std_logic;               -- rdatab(11)
-    p124 : in std_logic;                -- quadrant 11 (unused)
-    p19  : in std_logic;                -- quadrant 10 (unused)
-    p17  : in std_logic;                -- quadrant 01 (unused)
-    p15  : in std_logic;                -- quadrant 00 (unused)
-    p21  : in std_logic;                -- write
-    p23  : in std_logic);               -- read
+    p22 : in std_logic;                  -- addr(0)
+    p20 : in std_logic;                  -- addr(1)
+    p8 : in std_logic;                   -- addr(2)
+    p10 : in std_logic;                  -- addr(3)
+    p107   : in std_logic;               -- addr(4)
+    p109   : in std_logic;               -- addr(5)
+    p113  : in std_logic;                -- addr(6)
+    p111  : in std_logic;                -- addr(7)
+    p12 : in std_logic;                  -- addr(8)
+    p14 : in std_logic;                  -- addr(9)
+    p18 : in std_logic;                  -- addr(10)
+    p16 : in std_logic;                  -- addr(11)
+    p129  : in std_logic;                -- wdata(0)
+    p105   : in std_logic;               -- wdata(1)
+    p6 : in std_logic;                   -- wdata(2)
+    p26 : in std_logic;                  -- wdata(3)
+    p127  : in std_logic;                -- wdata(4)
+    p101   : in std_logic;               -- wdata(5)
+    p28 : in std_logic;                  -- wdata(6)
+    p4 : in std_logic;                   -- wdata(7)
+    p103   : in std_logic;               -- wdata(8)
+    p125  : in std_logic;                -- wdata(9)
+    p30 : in std_logic;                  -- wdata(10)
+    p2 : in std_logic;                   -- wdata(11)
+    p1 : out std_logic;                  -- rdata(0)
+    p3 : out std_logic;                  -- rdatab(0)
+    p27 : out std_logic;                 -- rdata(1)
+    p29 : out std_logic;                 -- rdatab(1)
+    p128  : out std_logic;               -- rdata(2)
+    p130  : out std_logic;               -- rdatab(2)
+    p102   : out std_logic;              -- rdata(3)
+    p104   : out std_logic;              -- rdatab(3)
+    p5 : out std_logic;                  -- rdata(4)
+    p7 : out std_logic;                  -- rdatab(4)
+    p23 : out std_logic;                 -- rdata(5)
+    p25 : out std_logic;                 -- rdatab(5)
+    p106   : out std_logic;              -- rdata(6)
+    p108   : out std_logic;              -- rdatab(6)
+    p124  : out std_logic;               -- rdata(7)
+    p126  : out std_logic;               -- rdatab(7)
+    p19 : out std_logic;                 -- rdata(8)
+    p21 : out std_logic;                 -- rdatab(8)
+    p9 : out std_logic;                  -- rdata(9)
+    p11 : out std_logic;                 -- rdatab(9)
+    p110  : out std_logic;               -- rdata(10)
+    p112  : out std_logic;               -- rdatab(10)
+    p120  : out std_logic;               -- rdata(11)
+    p122  : out std_logic;               -- rdatab(11)
+    p24 : in std_logic;                  -- quadrant 11 (unused)
+    p119  : in std_logic;                -- quadrant 10 (unused)
+    p117  : in std_logic;                -- quadrant 01 (unused)
+    p115  : in std_logic;                -- quadrant 00 (unused)
+    p121  : in std_logic;                -- write
+    p123  : in std_logic);               -- read
 end mem;
 
 architecture beh of mem is
@@ -124,55 +124,55 @@ architecture beh of mem is
 begin  -- beh
 
   ar : memarray port map (
-    addr(0) => p122,
-    addr(1) => p120,
-    addr(2) => p108,
-    addr(3) => p110,
-    addr(4) => p7,
-    addr(5) => p9,
-    addr(6) => p13,
-    addr(7) => p11,
-    addr(8) => p112,
-    addr(9) => p114,
-    addr(10) => p118,
-    addr(11) => p116,
-    wdata(0) => p29,
-    wdata(1) => p5,
-    wdata(2) => p106,
-    wdata(3) => p126,
-    wdata(4) => p27,
-    wdata(5) => p1,
-    wdata(6) => p128,
-    wdata(7) => p104,
-    wdata(8) => p3,
-    wdata(9) => p25,
-    wdata(10) => p130,
-    wdata(11) => p102,
-    rdata(0) => p101,
-    rdata(1) => p127,
-    rdata(2) => p28,
-    rdata(3) => p2,
-    rdata(4) => p105,
-    rdata(5) => p123,
-    rdata(6) => p6,
-    rdata(7) => p24,
-    rdata(8) => p119,
-    rdata(9) => p109,
-    rdata(10) => p10,
-    rdata(11) => p20,
-    rdatab(0) => p103,
-    rdatab(1) => p129,
-    rdatab(2) => p30,
-    rdatab(3) => p4,
-    rdatab(4) => p107,
-    rdatab(5) => p125,
-    rdatab(6) => p8,
-    rdatab(7) => p26,
-    rdatab(8) => p121,
-    rdatab(9) => p111,
-    rdatab(10) => p12,
-    rdatab(11) => p22,
-    write => p21,
-    read => p21);
+    addr(0) => p22,
+    addr(1) => p20,
+    addr(2) => p8,
+    addr(3) => p10,
+    addr(4) => p107,
+    addr(5) => p109,
+    addr(6) => p113,
+    addr(7) => p111,
+    addr(8) => p12,
+    addr(9) => p14,
+    addr(10) => p18,
+    addr(11) => p16,
+    wdata(0) => p129,
+    wdata(1) => p105,
+    wdata(2) => p6,
+    wdata(3) => p26,
+    wdata(4) => p127,
+    wdata(5) => p101,
+    wdata(6) => p28,
+    wdata(7) => p4,
+    wdata(8) => p103,
+    wdata(9) => p125,
+    wdata(10) => p30,
+    wdata(11) => p2,
+    rdata(0) => p1,
+    rdata(1) => p27,
+    rdata(2) => p128,
+    rdata(3) => p102,
+    rdata(4) => p5,
+    rdata(5) => p23,
+    rdata(6) => p106,
+    rdata(7) => p124,
+    rdata(8) => p19,
+    rdata(9) => p9,
+    rdata(10) => p110,
+    rdata(11) => p120,
+    rdatab(0) => p3,
+    rdatab(1) => p29,
+    rdatab(2) => p130,
+    rdatab(3) => p104,
+    rdatab(4) => p7,
+    rdatab(5) => p25,
+    rdatab(6) => p108,
+    rdatab(7) => p126,
+    rdatab(8) => p21,
+    rdatab(9) => p11,
+    rdatab(10) => p112,
+    rdatab(11) => p122,
+    write => p121,
+    read => p121);
 
 end beh;
