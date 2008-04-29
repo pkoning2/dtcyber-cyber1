01bf05cbb34b711e1f4423a010fae1d94e80bf2f
Paul Koning <ni1d@arrl.net>
2008-04-29 16:20:18 +0000

    tweak
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@891 d8f3d347-7192-4e35-a67b-6d701703d080

--- vhdl/pu.vhd
+++ vhdl/pu.vhd
@@ -20,7 +20,7 @@ entity pu is
     p10, p11, p17, p19, p18           : in  std_logic;
     tp1, tp5, tp6                     : out std_logic;  -- test points
     p9, p8, p1, p4, p3, p6, p7        : out std_logic;
-    p14, p16, p15                     : out std_logic;
+    p14, p16, p15                     : inout misc;  -- analog stuff
     p22, p21, p28, p27, p26, p25, p24 : out std_logic);
 
 end pu;
