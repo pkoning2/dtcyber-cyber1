3546761f3b25fcb2bcb2f25178266a8ccccb3d80
Paul Koning <ni1d@arrl.net>
2008-05-05 15:27:33 +0000

    More modules
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@925 d8f3d347-7192-4e35-a67b-6d701703d080

--- vhdl/tf.vhd
+++ vhdl/tf.vhd
@@ -6,9 +6,63 @@
 --
 -- Based on the original design by Seymour Cray and his team
 --
--- TF module
+-- TF module, rev B
 --
 -------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use work.sigs.all;
+
+entity tfslice is
+  
+  port (
+    i1, i2, i3, i4 : in  std_logic;     -- inputs
+    a, b, c, d     : in  std_logic;     -- selects
+    e              : in  std_logic;
+    y              : out std_logic);
+
+end tfslice;
+
+architecture gates of tfslice is
+  component g2
+    port (
+      a, b : in  std_logic;                   -- inputs
+      y, y2   : out std_logic);                  -- output
+  end component;
+  component g5
+    port (
+      a, b, c, d, e : in  std_logic;          -- inputs
+      y, y2   : out std_logic);                  -- output
+  end component;
+  signal t1, t2, t3, t4 : std_logic;
+begin  -- gates
+
+  u1 : g2 port map (
+    a => i1,
+    b => a,
+    y => t1);
+  u2 : g2 port map (
+    a => i2,
+    b => b,
+    y => t2);
+  u3 : g2 port map (
+    a => i3,
+    b => c,
+    y => t3);
+  u4 : g2 port map (
+    a => i4,
+    b => d,
+    y => t4);
+  u5 : g5 port map (
+    a => t1,
+    b => t2,
+    c => t3,
+    d => t4,
+    e => e,
+    y => y);
+
+end gates;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -24,3 +78,83 @@ entity tf is
     p13, p11, p19                      : out std_logic);
 
 end tf;
+
+architecture gates of tf is
+  component inv
+    port (
+      a  : in  std_logic;                     -- input
+      y  : out std_logic);                    -- output
+  end component;
+  component g3
+    port (
+      a, b, c : in  std_logic;                -- inputs
+      y, y2   : out std_logic);                  -- output
+  end component;
+  component tfslice
+    port (
+      i1, i2, i3, i4 : in  std_logic;     -- inputs
+      a, b, c, d     : in  std_logic;     -- selects
+      e              : in  std_logic;
+      y              : out std_logic);
+  end component;
+  signal a, b, c, d, e, f, g, t1 : std_logic;
+begin  -- gates
+
+  u1 : inv port map (
+    a => p1,
+    y => a);
+  u2 : inv port map (
+    a => p27,
+    y => b);
+  u3 : inv port map (
+    a => p2,
+    y => c);
+  u4 : inv port map (
+    a => p28,
+    y => d);
+  u5 : tfslice port map (
+    i1 => p8,
+    i2 => p22,
+    i3 => p7,
+    i4 => p21,
+    a  => a,
+    b  => b,
+    c  => c,
+    d  => d,
+    e  => p9,
+    y  => e);
+  tp5 <= e;
+  u6 : tfslice port map (
+    i1 => p6,
+    i2 => p24,
+    i3 => p5,
+    i4 => p23,
+    a  => a,
+    b  => b,
+    c  => c,
+    d  => d,
+    e  => p20,
+    y  => f);
+  tp6 <= f;
+  u7 : tfslice port map (
+    i1 => p4,
+    i2 => p26,
+    i3 => p3,
+    i4 => p25,
+    a  => a,
+    b  => b,
+    c  => c,
+    d  => d,
+    e  => p10,
+    y  => g);
+  u8 : g3 port map (
+    a => e,
+    b => f,
+    c => g,
+    y => t1);
+  p13 <= t1;
+  tp1 <= t1;
+  p11 <= t1;
+  p19 <= t1;
+
+end gates;
--- vhdl/tj.vhd
+++ vhdl/tj.vhd
@@ -6,9 +6,59 @@
 --
 -- Based on the original design by Seymour Cray and his team
 --
--- TJ module - two port mux
+-- TJ module, rev B - two port mux
 --
 -------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use work.sigs.all;
+
+entity tjslice is
+  
+  port (
+    a      : in  std_logic;             -- select
+    b, c   : in  std_logic;             -- inputs
+    tp     : out std_logic;             -- test point
+    y1, y2 : out std_logic);            -- outputs
+
+end tjslice;
+
+architecture gates of tjslice is
+  component inv2
+    port (
+      a  : in  std_logic;                     -- input
+      y, y2 : out std_logic);                    -- output
+  end component;
+  component g2
+    port (
+      a, b : in  std_logic;                   -- inputs
+      y, y2   : out std_logic);                  -- output
+  end component;
+  signal t1, t2, t3, t4, t5 : std_logic;
+begin  -- gates
+
+  u1 : inv2 port map (
+    a  => a,
+    y  => t1,
+    y2 => t2);
+  tp <= t2;
+  u2 : g2 port map (
+    a => t1,
+    b => b,
+    y => t3);
+  u3 : g2 port map (
+    a => t2,
+    b => c,
+    y => t4);
+  u4 : g2 port map (
+    a => t3,
+    b => t4,
+    y => t5);
+  y1 <= t5;
+  y2 <= t5;
+  
+end gates;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -24,3 +74,44 @@ entity tj is
     p27, p20, p19, p17           : out std_logic);
 
 end tj;
+
+architecture gates of tj is
+  component tjslice
+    port (
+      a      : in  std_logic;             -- select
+      b, c   : in  std_logic;             -- inputs
+      tp     : out std_logic;             -- test point
+      y1, y2 : out std_logic);            -- outputs
+  end component;
+begin  -- gates
+
+  u1 : tjslice port map (
+    a  => p10,
+    b  => p9,
+    c  => p8,
+    tp => tp1,
+    y1 => p12,
+    y2 => p14);
+  u2 : tjslice port map (
+    a  => p7,
+    b  => p6,
+    c  => p5,
+    tp => tp2,
+    y1 => p4,
+    y2 => p11);
+  u3 : tjslice port map (
+    a  => p24,
+    b  => p25,
+    c  => p26,
+    tp => tp5,
+    y1 => p27,
+    y2 => p20);
+  u4 : tjslice port map (
+    a  => p21,
+    b  => p22,
+    c  => p23,
+    tp => tp6,
+    y1 => p19,
+    y2 => p17);
+  
+end gates;
