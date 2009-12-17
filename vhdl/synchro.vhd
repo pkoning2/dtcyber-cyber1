cc44e2ee4eb44da34ad1ee247c9aa8939119200c
Paul Koning <ni1d@arrl.net>
2009-12-17 19:24:31 +0000

    optimize the Makefile so it doesn't remake everything whenever cyberdefs.vhd changes
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@1307 d8f3d347-7192-4e35-a67b-6d701703d080

--- vhdl/synchro.vhd
+++ vhdl/synchro.vhd
@@ -2,9 +2,14 @@
 --
 -- CDC 6600 model
 --
--- Authors: Paul Koning, Dave Redell, Al Kossow
+-- Copyright (C) 2008-2009 by Paul Koning
 --
--- Based on the original design by Seymour Cray and his team
+-- Derived from the original 6600 module design
+-- by Seymour Cray and his team at Control Data,
+-- as documented in CDC 6600 "Chassis Tabs" manuals,
+-- which are in the public domain.  Scans supplied
+-- from the Computer History Museum collection
+-- by Dave Redell and Al Kossow.
 --
 -------------------------------------------------------------------------------
 
@@ -24,29 +29,35 @@ entity synchro is
 end synchro;
 
 architecture synchro of synchro is
-
+  signal ocv : coaxsigs := idlecoax;
 begin  -- synchro
-
+  -- purpose: input latch
+  -- type   : combinational
+  -- inputs : p3, p2(16)
+  -- outputs: ocv
+  ilatch: process (p3, p2(16))
+  begin  -- process ilatch
+    if p2(16) = '1' then
+      ocv <= idlecoax;
+    end if;
+    for i in p3'range loop
+      if p3(i) = '1' then
+        ocv(i) <= '1';
+      end if;
+    end loop;
+  end process ilatch;
   -- purpose: Simulate 6600 synchronizer, connected to DtCyber
   -- type   : sequential
-  -- inputs : p2, p3
+  -- inputs : p2, ocv
   -- outputs: p1
-  sync: process
-    variable icv : coaxsigs;
-    variable ocv : coaxsigs;
+  sync: process (p2(16))
+    variable icv, t : coaxsigs;
   begin  -- process
-    wait on p2(16);
-    if p2(16)'event and p2(16) = '0' then
-      ocv := p3;
-      wait for 10 ns;
-      for i in ocv'range loop
-        ocv(i) := ocv(i) or p3(i);
-      end loop;  -- i
-      
+    if p2(16)'event and p2(16) = '1' then
       icv := idlecoax;
-      dtsynchro (chnum, icv, ocv);
+      t := ocv;
+      dtsynchro (chnum, icv, t);
       p1 <= icv, idlecoax after 25 ns;
-      
     end if;
   end process;
 
