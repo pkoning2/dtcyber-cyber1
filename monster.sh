3efc6dba980f63c426b200446e0f5b3963b1448b
Paul Koning <ni1d@arrl.net>
2010-04-26 13:45:04 +0000

    Add tunnel for CHM
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@1461 d8f3d347-7192-4e35-a67b-6d701703d080

--- monster.sh
+++ monster.sh
@@ -4,4 +4,4 @@ if [ "$1" = "" ]; then
 else
     USERNAME="$1"
 fi
-ssh -X -L 5104:localhost:5004  -L 5105:localhost:5005  -L 5106:localhost:5006  -L 5107:localhost:5007 -L 8105:localhost:8005 -L 5204:localhost:5204  -L 5205:localhost:5205  -L 5206:localhost:5206  -L 5207:localhost:5207 -L 8205:localhost:8205  -L 5831:localhost:6021 -R 11851:localhost:6021 $USERNAME@cyberserv.org
+ssh -X -L 5104:localhost:5004  -L 5105:localhost:5005  -L 5150:localhost:5050  -L 5106:localhost:5006  -L 5107:localhost:5007 -L 8105:localhost:8005 -L 8150:localhost:8050 -L 5204:localhost:5204  -L 5205:localhost:5205  -L 5206:localhost:5206  -L 5207:localhost:5207 -L 8205:localhost:8205  -L 5831:localhost:6021 -R 11851:localhost:6021 $USERNAME@cyberserv.org
