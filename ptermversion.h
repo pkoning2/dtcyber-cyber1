4dfe87493602e8809983f062cca6628148cc6b44
Paul Koning <ni1d@arrl.net>
2012-02-23 11:59:27 +0000

    update version
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/branches/threadnet@1551 d8f3d347-7192-4e35-a67b-6d701703d080

--- ptermversion.h
+++ ptermversion.h
@@ -1,7 +1,7 @@
-#define FILEVER             4,1,11,0
-#define PRODUCTVER          4,1,11,0
-#define STRFILEVER          "4.1.11"
-#define STRPRODUCTVER       "4.1.11"
+#define FILEVER             4,1,12,0
+#define PRODUCTVER          4,1,12,0
+#define STRFILEVER          "4.1.12"
+#define STRPRODUCTVER       "4.1.12"
 #define STRFILEDESC         "PLATO Terminal Emulator"
 #define STRPRODUCTNAME      "PLATO Terminal Emulator"
 #define STRLEGALCOPYRIGHT   "Copyright \xA9 2004-2011, Paul Koning & Joe Stanton"
