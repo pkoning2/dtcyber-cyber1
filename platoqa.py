2d58bd8175edc5a93e632303989c3ac8e164b47f
Paul Koning <ni1d@arrl.net>
2012-02-27 19:52:32 +0000

    Some fixes and improvements
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/branches/threadnet@1563 d8f3d347-7192-4e35-a67b-6d701703d080

--- platoqa.py
+++ platoqa.py
@@ -10,6 +10,7 @@ import random
 import time
 import traceback
 import dtscript
+import re
 
 class QaOper (dtscript.Oper):
     def __init__ (self, idx):
@@ -35,6 +36,7 @@ class QaPterm (dtscript.Pterm):
         dtscript.Pterm.__init__ (self, "localhost", port)
         self.login ("diag", "m")
         diagopts = "abcdefghijkmopq"
+        diagopts = "aqnnnnnn"
         diagopt = diagopts[idx % len (diagopts)]
         self.sendstr ("a")
         self.waitarrow (5)
@@ -48,10 +50,14 @@ class QaPterm (dtscript.Pterm):
         #    self.logout ()
         dtscript.Pterm.stop (self)
 
-remote = raw_input ("Test via tunnel? ").lower ()[0] == 'y'
+remote = raw_input ("Test via tunnel? ").lower ()[:0] == 'y'
 ocnt = int (raw_input ("Number of dtoper connections? "))
 ccnt = int (raw_input ("Number of console connections? "))
 pcnt = int (raw_input ("Number of pterm connections? "))
+
+dd60conn = re.compile (r"DD6612.+? (\d+) connection", re.M)
+niuconn = re.compile (r"NIU.+?(\d+) connection", re.M)
+connre = re.compile (r"(\d+)\. (\w+)")
 
 qathings = [ ]
 try:
@@ -62,6 +68,7 @@ try:
                 qalist.append (c (i))
             qathings.append (qalist)
         print "all started up"
+        operlist = qathings[0]
         
         while True:
             time.sleep (2)
@@ -70,15 +77,41 @@ try:
                 try:
                     q = l[item]
                     c = q.__class__
-                    print c.__name__, item
-                    r = repr (q)
-                    if r.replace ("\n", "").replace (" ", ""):
-                        print r
+                    print "restarting", c.__name__, item
+                    #r = repr (q)
+                    #if r.replace ("\n", "").replace (" ", ""):
+                    #    print r
                     q.stop ()
                     l[item] = c (item)
-                    break
+                    if operlist and operlist[0]:
+                        op = operlist[0]
+                        op.command ("SHOW,DEVICES.")
+                        s = repr (op)
+                        m1 = dd60conn.search (s)
+                        m2 = niuconn.search (s)
+                        cons = niu = 0
+                        if m1:
+                            cons = int (m1.group (1))
+                        if m2:
+                            niu = int (m2.group (1))
+                        print "dev shows", op.operators, "op", \
+                              cons, "dd60", niu, "niu connections"
+                        op.command ("SHOW,CONNECTIONS.")
+                        ops = cons = niu = 0
+                        for m in connre.finditer (repr (op)):
+                            n = int (m.group (1))
+                            t = m.group (2)
+                            if t == "oper":
+                                ops += 1
+                            elif t == "dd60":
+                                cons += 1
+                            elif t == "remote":
+                                niu += 1
+                        print "conns shows", op.operators, "op", \
+                              cons, "dd60", niu, "niu connections, max", n
                 except IndexError:
                     item -= len (l)
+            sys.stdout.flush ()
     except KeyboardInterrupt:
         print
 finally:
