9ba999d95d587d4c7aa514af6dab1fabf3f885a1
Paul Koning <ni1d@arrl.net>
2014-12-17 22:00:45 +0000

    Tweaks for building that retina related stuff on the old PPC Mac
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/branches/pterm-5.0@1903 d8f3d347-7192-4e35-a67b-6d701703d080

--- wxutil.m
+++ wxutil.m
@@ -1,6 +1,14 @@
+#if defined (__ppc__) || defined (__ppc64__)
+
+float mainDisplayScale (void)
+{
+    return 1.0;
+}
+#else
 #import <AppKit/NSScreen.h>
 
 float mainDisplayScale (void)
 {
     return [[NSScreen mainScreen] backingScaleFactor];
 }
+#endif
