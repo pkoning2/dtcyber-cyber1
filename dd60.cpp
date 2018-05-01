3593eb5ba0d3306cda19af72704ae9800f40ad3b
Paul Koning <ni1d@arrl.net>
2018-05-01 18:42:03 +0000

    Fix for non-SSE builds
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@2380 d8f3d347-7192-4e35-a67b-6d701703d080

--- dd60.cpp
+++ dd60.cpp
@@ -1079,8 +1079,12 @@ Dd60Frame::Dd60Frame(int port, double interval, const wxString& title)
     
     trace_txt[0] = '\0';
 
+#ifdef __SSE2__
     sse2 = __builtin_cpu_supports ("sse2");
     avx2 = __builtin_cpu_supports ("avx2");
+#else
+    sse2 = avx2 = 0;
+#endif
     
     // Calculate scale factors and sizes
     m_pscale = GetContentScaleFactor ();
