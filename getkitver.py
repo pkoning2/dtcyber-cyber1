20ca948d15094f3088675765d609a3b8aed32090
Paul Koning <ni1d@arrl.net>
2019-05-28 16:32:50 +0000

    Fix getkitver script, stray executable flags -- thanks to Gary Buhrmaster
    git-svn-id: svn://akdesign.dyndns.org/dtcyber/trunk@2388 d8f3d347-7192-4e35-a67b-6d701703d080

--- getkitver.py
+++ getkitver.py
@@ -12,5 +12,5 @@ for l in f:
         nums2 = [ ]
         for n in nums:
             nums2.append (int (n))
-        print "%d.%d.%d" % tuple (nums2)[:3]
+        print ("%d.%d.%d" % tuple (nums2)[:3])
         sys.exit (0)
